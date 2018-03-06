9987e986cff164108875ac3d79243edb591b326f
Vinícius Batista Barreira <viniciustes@gmail.com>
2018-03-06 17:19:17 -0300

    Commit criação Dynamic Menu Item

---
+++ DrinkAndGo/Components/CategoryMenu.cs
@@ -0,0 +1,22 @@
+﻿using DrinkAndGo.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DrinkAndGo.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(x => x.CategoryName);
+            return View(categories);
+        }
+    }
+}
