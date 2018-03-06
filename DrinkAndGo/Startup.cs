3b1ef751ee34ca64f9487f33d326955ae51131f9
Vinícius Batista Barreira <viniciustes@gmail.com>
2018-03-06 16:59:14 -0300

    Commit da inclusão do Navigation Menu

--- DrinkAndGo/Startup.cs
+++ DrinkAndGo/Startup.cs
@@ -5,7 +5,6 @@ using DrinkAndGo.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
