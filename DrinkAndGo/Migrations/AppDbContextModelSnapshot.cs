1bbaa8aaec5e42f0245227ea750b5d809661216b
Vinícius Batista Barreira <viniciustes@gmail.com>
2018-03-06 15:49:29 -0300

    Commit da inclusão de Tag Helper

--- DrinkAndGo/Migrations/AppDbContextModelSnapshot.cs
+++ DrinkAndGo/Migrations/AppDbContextModelSnapshot.cs
@@ -3,10 +3,6 @@ using DrinkAndGo.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
-using System;
 
 namespace DrinkAndGo.Migrations
 {
